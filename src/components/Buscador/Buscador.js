afa3abd9a121f3592486bb839228529de6914dd7
jairoPonti <jairmponti@gmail.com>
2021-05-06 16:22:09 -0300

    avance front

---
+++ src/components/Buscador/Buscador.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from 'react-router-dom';
+import './Buscador.css';
+import { addMovieFavorite, getMovies } from "../../actions/index";
+
+
+
+export class Buscador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ""
+    };
+  }
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.getMovies(this.state.title);
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <div>
+        <h2>Películas o Series</h2>
+        <form className="form-container" onSubmit={(e) => this.handleSubmit(e)}>
+          <div>
+            <input
+              type="text"
+              id="title"
+              autoComplete="off"
+              value={title}
+              onChange={(e) => this.handleChange(e)}
+            />
+          </div>
+          <button type="submit">BUSCAR</button>
+        </form>
+       
+          <nav className="col-10 m-auto p-5">
+        <ul className="row">
+          {this.props.movies && this.props.movies.map((peli, i) => (
+            <div key={i} className="col p-3">
+              <img src={peli.Poster}/>
+              <div className="col">
+              <NavLink to={`/movie/${peli.imdbID}`}>
+                {peli.Title}
+              </NavLink>
+              </div>
+              <div className="col">
+              <button onClick={() => this.props.addMovieFavorite({ title: peli.Title, id: peli.imdbID })} style={{color: "white", backgroundColor: "#7124D3"}}>Fav</button>
+              </div>
+            </div>))}
+        </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    movies: state.moviesLoaded
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addMovieFavorite: movie => dispatch(addMovieFavorite(movie)),
+    getMovies: title => dispatch(getMovies(title))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Buscador);
+
+
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../cam.gif'
+
+import './Navbar.css';
+
+export default function NavBar() {
+    return (
+        <header className="navbar">
+            <div>
+                <img id="logo" src={Logo} width="85" height="85" className="d-inline-block align-top" alt="" />
+            </div>
+            <nav>
+                <ul className="list">
+                    <li className="list-item">
+                        <i className="material-icons" style={{fontSize: "40px", color: "#787D8F", marginRight:"14px"}} >home</i>
+                        <NavLink exact to="/" id="inicio" >INICIO</NavLink>
+                        <NavLink to="/favs" className="list" >Favoritas</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
