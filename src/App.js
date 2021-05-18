34aa67dc640613612dd8d116f9f02d8fbfb8826e
jairoPonti <jairmponti@gmail.com>
2021-05-18 01:24:02 -0300

    Front terminado

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Buscador from "./components/Buscador/Buscador";
 import NavBar from "./components/NavBar/NavBar";
 import { Route } from "react-router-dom";
 import Movie from "./components/Movie/Movie";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
       <React.Fragment>
           <NavBar />
           <Route exact path="/" component={Buscador} />
+          <Route exact path="/" component={Footer} />
           <Route path="/favs" component={Favorites} />
           <Route path="/movie/:id" component={Movie} />
       </React.Fragment>
